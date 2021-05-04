430dec16edf751c04ddac99594be9fec7b203f09
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-05-04 17:12:51 +0900

    Add some tests for lrxml/template/tokenize

---
+++ test/lrxml_template_tokenize.ts
@@ -0,0 +1,46 @@
+#!/usr/bin/env ts-node
+
+import tap from 'tap'
+
+import { parserContext } from '../src/context'
+
+import { parse } from '../src/lrxml/multipart/parse'
+import { tokenize } from '../src/lrxml/template/tokenize'
+
+// import { createInterface } from 'readline'
+
+const it = (source: string) => {
+    let ctx = parserContext({ source, config: {} })
+    return Array.from(parse(ctx)).map((part) => {
+        return {
+            part: part.kind, attlist: part.attlist.map((att) => ctx.range_text(att)),
+            tokens: Array.from(tokenize(ctx, part.payload)).map((tok) => {
+                if (tok.kind === "comment" && tok.contentRange != null) {
+                    return {
+                        kind: tok.kind, "text": ctx.range_text(tok),
+                        contentRange: ctx.range_text(tok.contentRange)
+                    }
+                } else {
+                    return { kind: tok.kind, "text": ctx.range_text(tok) }
+                }
+            })
+        }
+    })
+}
+
+tap.same(it(``), [])
+
+tap.same(it(`<!yatt:foo bar x y>
+content
+`), [
+    {
+        part: "foo",
+        attlist: [
+            "bar", "x", "y"
+        ],
+        tokens: [
+            {kind: "text", text: `content
+`}
+        ]
+    }
+])
