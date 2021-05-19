7e97f91ac17df2f1dd807378ca0e4691d65e672c
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-05-19 16:00:54 +0900

    Export AttItem type

--- packages/lrxml/src/index.ts
+++ packages/lrxml/src/index.ts
@@ -5,3 +5,5 @@ export {parserContext, Range, ParserContext} from './context'
 export {parse as parse_multipart, Part} from './multipart/parse'
 
 export {parse, Node} from './template/parse'
+
+export {AttItem} from './attlist/parse'
