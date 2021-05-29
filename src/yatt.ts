9a6cc1f42c4ece2234c8313ef8e6586de3a3b737
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-05-29 18:02:28 +0900

    build で書き直し

--- src/yatt.ts
+++ src/yatt.ts
@@ -6,18 +6,29 @@ import {
     parse
 } from 'lrxml-js'
 
-import {build_from_file} from './part-set/build'
+import {build} from './part-set/build'
 
 const [_cmd, _script, ...args] = process.argv;
 const debugLevel = parseInt(process.env.DEBUG ?? '', 10) || 0
 
 for (const fn of args) {
-
-    const part = build_from_file(fn, {
-        debug: {
-            parser: debugLevel
+    const { readFileSync } = require("fs")
+    let ctx = parserContext({
+        filename: fn, source: readFileSync(fn, {encoding: "utf-8"}), config: {
+            debug: {
+                parser: debugLevel
+            }
         }
     })
 
-    console.log(part)
+    const partSet = build(ctx)
+
+    for (const name of Object.keys(partSet)) {
+        const part = partSet[name]
+        if (part.type === "widget") {
+            parse(ctx, part.raw_part)
+        }
+    }
+
+    console.log(partSet)
 }
