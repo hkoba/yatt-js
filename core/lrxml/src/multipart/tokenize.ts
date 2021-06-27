e1c62bc8d0daa5fb1397bca807f6274a2aff73ff
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-06-27 15:54:16 +0900

    Reformat

--- core/lrxml/src/multipart/tokenize.ts
+++ core/lrxml/src/multipart/tokenize.ts
@@ -33,69 +33,69 @@ function re_decl_open(ns: string[]): RegExp {
   return new RegExp(pat, 'g')
 }
 
-export type ChunkGenerator = Generator<Chunk, any, any>
+export type ChunkGenerator = Generator<Chunk, any, any>;
 
-  export function* tokenize(ctx: ParserContext): ChunkGenerator {
-    let re_decls = ctx.re('decls', () => re_decl_open(ctx.session.params.namespace))
-    let re_comment_end = ctx.session.params.compat_end_of_comment ?
-      /(?<prefix>.*?)-->/sy :
-      /(?<prefix>.*?)#-->/sy;
-    let re_decl_end = />\r?\n/y;
-    
-    let globalMatch: GlobalMatch | null = null
-    while ((globalMatch = ctx.global_match(re_decls)) !== null) {
+export function* tokenize(ctx: ParserContext): ChunkGenerator {
+  let re_decls = ctx.re('decls', () => re_decl_open(ctx.session.params.namespace))
+  let re_comment_end = ctx.session.params.compat_end_of_comment ?
+    /(?<prefix>.*?)-->/sy :
+    /(?<prefix>.*?)#-->/sy;
+  let re_decl_end = />\r?\n/y;
+  
+  let globalMatch: GlobalMatch | null = null
+  while ((globalMatch = ctx.global_match(re_decls)) !== null) {
 
-      const prefix = ctx.prefix_of(globalMatch)
-      if (prefix != null) {
-        yield { kind: "text", ...prefix }
-      }
-
-      if (globalMatch.match.groups == null) continue
-
-      const dm: DeclMatch = globalMatch.match.groups
-
-      if (dm.comment != null) {
-        ctx.tab_string(globalMatch.match[0])
-        const end = ctx.match_index(re_comment_end)
-        if (!end || !end.groups) {
-          ctx.throw_error("Comment is not closed by '#-->'!", { index: globalMatch.match.index })
-        }
-        const innerRange = ctx.contained_string_range(globalMatch, end.groups.prefix)
-        if (innerRange == null) {
-          ctx.NEVER()
-        }
-        const comment = ctx.tab(globalMatch, end)
-        yield { kind: "comment", innerRange, ...comment }
-      } else if (dm.declname != null) {
-        // <!yatt:widget ...
-        yield {
-          kind: "decl_begin", detail: dm.declname,
-          lineNo: ctx.line_number(globalMatch.match.index),
-          ...ctx.tab(globalMatch)
-        }
-
-        // name name="value" name=[name name="value"...]...
-        yield* tokenize_attlist(ctx, '%')
-
-        // console.log("REST: ", ctx.remainder(3))
-
-        const end = ctx.match_index(re_decl_end)
-        if (!end) {
-          // XXX: yatt vs lrxml
-          ctx.throw_error("yatt declaration is not closed", { index: globalMatch.match.index })
-        }
-
-        yield { kind: "decl_end", ...ctx.tab_string(end[0]) }
-      } else {
-        ctx.throw_error("Unknown case!")
-      }
+    const prefix = ctx.prefix_of(globalMatch)
+    if (prefix != null) {
+      yield { kind: "text", ...prefix }
     }
-    
-    const rest = ctx.rest_range()
-    if (rest != null) {
-      yield {kind: "text", ...rest}
+
+    if (globalMatch.match.groups == null) continue
+
+    const dm: DeclMatch = globalMatch.match.groups
+
+    if (dm.comment != null) {
+      ctx.tab_string(globalMatch.match[0])
+      const end = ctx.match_index(re_comment_end)
+      if (!end || !end.groups) {
+        ctx.throw_error("Comment is not closed by '#-->'!", { index: globalMatch.match.index })
+      }
+      const innerRange = ctx.contained_string_range(globalMatch, end.groups.prefix)
+      if (innerRange == null) {
+        ctx.NEVER()
+      }
+      const comment = ctx.tab(globalMatch, end)
+      yield { kind: "comment", innerRange, ...comment }
+    } else if (dm.declname != null) {
+      // <!yatt:widget ...
+      yield {
+        kind: "decl_begin", detail: dm.declname,
+        lineNo: ctx.line_number(globalMatch.match.index),
+        ...ctx.tab(globalMatch)
+      }
+
+      // name name="value" name=[name name="value"...]...
+      yield* tokenize_attlist(ctx, '%')
+
+      // console.log("REST: ", ctx.remainder(3))
+
+      const end = ctx.match_index(re_decl_end)
+      if (!end) {
+        // XXX: yatt vs lrxml
+        ctx.throw_error("yatt declaration is not closed", { index: globalMatch.match.index })
+      }
+
+      yield { kind: "decl_end", ...ctx.tab_string(end[0]) }
+    } else {
+      ctx.throw_error("Unknown case!")
     }
   }
+  
+  const rest = ctx.rest_range()
+  if (rest != null) {
+    yield {kind: "text", ...rest}
+  }
+}
 
 if (module.id === ".") {
   const { readFileSync } = require("fs")
