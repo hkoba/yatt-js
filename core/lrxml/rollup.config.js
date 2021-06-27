9a481882bb7f65fba6c47ec262365d54a1060047
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-06-27 14:10:13 +0900

    Change js-indent-level to 2

--- core/lrxml/rollup.config.js
+++ core/lrxml/rollup.config.js
@@ -8,39 +8,39 @@ import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 import pkg from './package.json';
 
 export default [
-    // browser-friendly UMD build
-    {
-	input: 'src/index.ts',
-	output: {
-	    name: 'lrxml',
-	    file: pkg.browser,
-	    format: 'umd',
-            sourcemap: true
-	},
-	plugins: [
-            preserveShebangs(),
-	    resolve(),   // so Rollup can find external modules
-	    commonjs(),  // so Rollup can convert external modules to an ES module
-	    typescript() // so Rollup can convert TypeScript to JavaScript
-	]
+  // browser-friendly UMD build
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'lrxml',
+      file: pkg.browser,
+      format: 'umd',
+      sourcemap: true
     },
+    plugins: [
+      preserveShebangs(),
+      resolve(),   // so Rollup can find external modules
+      commonjs(),  // so Rollup can convert external modules to an ES module
+      typescript() // so Rollup can convert TypeScript to JavaScript
+    ]
+  },
 
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify 
-    // `file` and `format` for each target)
-    {
-	input: 'src/index.ts',
-	external: [],
-	plugins: [
-            preserveShebangs(),
-	    typescript() // so Rollup can convert TypeScript to JavaScript
-	],
-	output: [
-	    { file: pkg.main, format: 'cjs', sourcemap: true },
-	    { file: pkg.module, format: 'es', sourcemap: true }
-	]
-    }
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify 
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.ts',
+    external: [],
+    plugins: [
+      preserveShebangs(),
+      typescript() // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ]
+  }
 ];
