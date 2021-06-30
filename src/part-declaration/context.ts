a980cedd64a1b6f211245844bb295c8f033e0eef
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-06-30 19:39:02 +0900

    Define YattConfig & YattParams

--- src/part-declaration/context.ts
+++ src/part-declaration/context.ts
@@ -1,11 +1,33 @@
 import {
-  yattParams, YattConfig,
+  LrxmlParams,
   ParserSession,
   ScanningContext,
   RawPart, AttItem
 } from 'lrxml-js'
 
 import { Part, ArgDict, DefaultFlag } from './part'
+
+export type YattParams = LrxmlParams
+export type YattConfig = Partial<YattParams>
+
+export function yattParams(config: YattConfig): YattParams {
+  let {
+    namespace = ["yatt"],
+    doc_root = "",
+    default_part = "page",
+    compat_end_of_comment = false,
+    debug = {}
+  } = config;
+
+  return {
+    namespace,
+    doc_root,
+    default_part,
+    compat_end_of_comment,
+    debug
+  }
+}
+
 
 export type BuilderMap = Map<string, DeclarationBuilder>;
 
