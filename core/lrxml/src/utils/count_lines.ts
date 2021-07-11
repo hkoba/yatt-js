249b2d096ec42f65557e3788d25bfdbdc7881c2e
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-07-11 13:54:21 +0900

    Add extract_line and its tests

--- core/lrxml/src/utils/count_lines.ts
+++ core/lrxml/src/utils/count_lines.ts
@@ -8,3 +8,13 @@ export function lineNumber(str: string): number {
 export function count_newlines(str: string): number {
   return str.length - str.replace(/\n/g, "").length;
 }
+
+export function extract_line(str: string, lastNl: number, colNo: number): string {
+  const startOfLine = lastNl+1;
+  const bottomHalf = str.substring(startOfLine)
+  const nextNl = bottomHalf.indexOf('\n')
+  const line = str.substring(lastNl, nextNl >= 0 ? startOfLine + nextNl : undefined)
+  // console.log(`lastNl: ${lastNl} nextNl: ${nextNl} colNo: ${colNo} line: ${line}`);
+  const notab = line.replace(/[^\t]/g, ' ');
+  return line + '\n' + notab.substr(0, colNo-1) + '^';
+}
---
+++ core/lrxml/test/count_lines.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node
+
+const tap = require('tap')
+
+import {lineNumber, extract_line} from '../src/utils/count_lines'
+
+tap.same(lineNumber(``), 1)
+
+tap.same(lineNumber(`foo`), 1)
+
+tap.same(lineNumber(`foo
+bar`), 2)
+
+tap.same(lineNumber(`foo
+bar
+`), 3)
+
+tap.same(extract_line(`01234`, 0, 2), `01234` + `\n ^`);
+
+tap.same(extract_line(`foo
+bar
+`, 4, 2), `bar` + `\n ^`);
