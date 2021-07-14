2d58042a9f51501043f679ce7b156a949f9aad8d
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-07-14 16:37:45 +0900

    Add more tests for attlist tokens

--- core/lrxml/test/template_tokenize.ts
+++ core/lrxml/test/template_tokenize.ts
@@ -7,25 +7,25 @@
 import tap from 'tap'
 
 import { ParserSession, range_text } from '../src/context'
-import {AttItem} from '../src/attlist/parse'
+import {AttItem, hasLabel} from '../src/attlist/parse'
 import { parse_multipart } from '../src/multipart/parse'
 import { tokenize } from '../src/template/tokenize'
 
 // import { createInterface } from 'readline'
-const srcText = (session: ParserSession, att: AttItem) => 
-  range_text(session.source, att);
 
 const it = (source: string) => {
   let [partList, session] = parse_multipart(source, {});
   return Array.from(partList).map((part) => {
     return {
       part: part.kind,
-      attlist: part.attlist.map((att) => [
-        att.kind,
-        (att.label
-         ? [srcText(session, att.label), srcText(session, att)]
-         : srcText(session, att))
-      ]),
+      attlist: part.attlist.map(att => {
+        if (hasLabel(att)) {
+          return [att.label.kind, att.label.value,
+                  att.kind, (att as any).value]
+        } else {
+          return [att.kind, (att as any).value]
+        }
+      }),
       tokens: Array.from(tokenize(session, part.payload)).map((tok) => {
         if (tok.kind === "comment" && tok.innerRange != null) {
           return {
@@ -56,3 +56,22 @@ content
     ]
   }
 ])
+
+tap.same(it(`<!yatt:foo bar x=3 y="aaa" z='bbb'>
+content
+`), [
+  {
+    part: "foo",
+    attlist: [
+      ["identplus", "bar"],
+      ["identplus", "x", "bare", 3],
+      ["identplus", "y", "dq",   "aaa"],
+      ["identplus", "z", "sq",   "bbb"],
+    ],
+    tokens: [
+      {kind: "text", text: `content
+`}
+    ]
+  }
+])
+
