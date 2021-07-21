c0c1004fb412b1449752ceffa2a91facb7085ab4
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-07-21 17:04:48 +0900

    Refactor build_delegate_variable_adder

--- src/declaration/build.ts
+++ src/declaration/build.ts
@@ -235,53 +235,18 @@ function add_args_cont(
           else {
             let [typeName, ...restName] = fst.value.split(/:/)
             if (typeName === "delegate") {
-              return {
-                name: part.name, dep: restName.length ? restName.join(":") : name,
-                fun: (widget: Widget): ArgAdder | undefined => {
-                  let v: DelegateVar = {
-                    typeName: "delegate", varName: name,
-                    widget,
-                    delegateVars: new Map,
-                    attItem: att, argNo: part.argMap.size,
-                    is_callable: true, from_route: false,
-                    is_body_argument: false,
-                    is_escaped: false
-                  }
-
-                  part.varMap.set(att.label.value, v)
-
-                  if (attlist.length) {
-                    for (const att of attlist) {
-                      if (! isIdentOnly(att)) {
-                        ctx.NIMPL()
-                      }
-                      let name = att.value
-                      if (! widget.argMap.has(name)) {
-                        ctx.throw_error(`No such argument ${name} in delegated widget ${widget.name}`)
-                      }
-                      // XXX: deep copy, with original link?
-                      part.argMap.set(name, widget.argMap.get(name)!)
-                    }
-                  } else {
-                    for (const [name, value] of widget.argMap.entries()) {
-                      if (part.argMap.has(name)) {
-                        if (ctx.debug) {
-                          // XXX: better diag
-                          console.log(`skipping ${name} because it already exists`)
-                        }
-                        continue
-                      }
-                      part.argMap.set(name, value)
-                    }
-                  }
-                  return add_args_cont(ctx, part, gen)
-                }
-              }
+              return build_delegate_variable_adder(
+                ctx, part, gen, att, part.argMap.size,
+                name, restName, attlist
+              )
             }
             else {
               ctx.token_error(att, `Unknown arg decl`)
             }
           }
+        }
+        else {
+          ctx.token_error(att, `Unknown arg declaration`)
         }
       }
       else {
@@ -310,6 +275,55 @@ function build_widget_varialbe(ctx: BuilderContext, att: AttItem, argNo: number,
     is_callable: true, from_route: false,
     is_body_argument: ctx.is_body_argument(varName),
     is_escaped: false
+  }
+}
+
+function build_delegate_variable_adder(
+  ctx: BuilderContext, part: Part, gen: Generator<AttItem>,
+  att: AttItem, argNo: number,
+  name: string, restName: string[], attlist: AttItem[]
+): ArgAdder {
+  return {
+    name: part.name, dep: restName.length ? restName.join(":") : name,
+    fun: (widget: Widget): ArgAdder | undefined => {
+      let v: DelegateVar = {
+        typeName: "delegate", varName: name,
+        widget,
+        delegateVars: new Map,
+        attItem: att, argNo,
+        is_callable: true, from_route: false,
+        is_body_argument: false,
+        is_escaped: false
+      }
+
+      part.varMap.set(name, v)
+
+      if (attlist.length) {
+        for (const att of attlist) {
+          if (! isIdentOnly(att)) {
+            ctx.NIMPL()
+          }
+          let name = att.value
+          if (! widget.argMap.has(name)) {
+            ctx.throw_error(`No such argument ${name} in delegated widget ${widget.name}`)
+          }
+          // XXX: deep copy, with original link?
+          part.argMap.set(name, widget.argMap.get(name)!)
+        }
+      } else {
+        for (const [name, value] of widget.argMap.entries()) {
+          if (part.argMap.has(name)) {
+            if (ctx.debug) {
+              // XXX: better diag
+              console.log(`skipping ${name} because it already exists`)
+            }
+            continue
+          }
+          part.argMap.set(name, value)
+        }
+      }
+      return add_args_cont(ctx, part, gen)
+    }
   }
 }
 
