f8cecbf653cb333781c38c59999ccf31f9de2006
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-07-22 15:27:26 +0900

    Update to current interfaces

--- src/yatt-clasp.ts
+++ src/yatt-clasp.ts
@@ -7,8 +7,9 @@ import path from 'path'
 
 const pGlob = promisify(glob)
 
-import { parserContext, parse } from 'lrxml-js'
-import {build} from './part-set/build'
+import { parse_long_options, parse_template } from 'lrxml-js'
+
+import { build_template_declaration, BuilderContext } from './declaration/'
 
 import {generate_widget} from './codegen'
 
@@ -18,63 +19,65 @@ const debugLevel = parseInt(process.env.DEBUG ?? '', 10) || 0;
 const baseNS = 'templates';
 
 function rootname(fn: string): string {
-    const ext = path.extname(fn)
-    if (ext === '') {
-        return fn
-    } else {
-        return fn.substring(0, fn.length - ext.length)
-    }
+  const ext = path.extname(fn)
+  if (ext === '') {
+    return fn
+  } else {
+    return fn.substring(0, fn.length - ext.length)
+  }
 }
 
 
 (async () => {
-    for (const tmplDir of args) {
-        const fileList = await pGlob('**/*.ytjs', {cwd: tmplDir, root: tmplDir})
-        for (const fn of fileList) {
-            const nsPrefix = baseNS + '.' + rootname(fn).split('/').join('.')
-            process.stdout.write(`namespace ${nsPrefix} {\n`);
-            let ctx = parserContext({
-                filename: fn, source: readFileSync(
-                    path.resolve(tmplDir, fn), {encoding: "utf-8"}
-                ),
-                config: {
-                    debug: {parser: debugLevel}
-                }
-            })
+  const debugLevel = parseInt(process.env.DEBUG ?? '', 10) || 0
+  let config = {
+    body_argument_name: "body",
+    debug: { declaration: debugLevel }
+  }
+  parse_long_options(args, {target: config})
 
-            const partSet = build(ctx)
-            for (const name of Object.keys(partSet)) {
-                const part = partSet[name]
+  // const { readFileSync } = require('fs')
 
-                switch (part.type) {
-                    case "widget": {
-                        const ast = parse(ctx, part.raw_part)
-                        const script = generate_widget(ctx, name, part, ast)
-                        process.stdout.write(script);
-                        break;
-                    }
-                    case "entity": {
-                        process.stdout.write(`export function entity_${name} {\n`)
-                        for (const item of part.raw_part.payload) {
-                            console.log(item)
-                        }
+  for (const tmplDir of args) {
+    const fileList = await pGlob('**/*.ytjs', {cwd: tmplDir, root: tmplDir})
+    for (const fn of fileList) {
+      const nsPrefix = baseNS + '.' + rootname(fn).split('/').join('.')
+      process.stdout.write(`namespace ${nsPrefix} {\n`);
 
-                        process.stdout.write(`}\n`);
-                        break;
-                    }
-                    case "action": {
-                        process.stdout.write(`export function do_${name} {\n`)
-                        for (const item of part.raw_part.payload) {
-                            console.log(item)
-                        }
-                        process.stdout.write(`}\n`);
-                        break;
-                    }
-                    default:
-                }
+      const [template, session] = build_template_declaration(
+        readFileSync(path.resolve(tmplDir, fn), {encoding: "utf-8"}),
+        {filename: fn, ...config}
+      );
 
-            }
-            process.stdout.write(`}\n`);
+      const ctx = new BuilderContext(session)
+
+      for (const [kind, name] of template.partOrder) {
+        const partMap = template.partMap[kind]
+        if (partMap == null)
+          throw new Error(`BUG: Unknown part kind ${kind}`)
+        const part = partMap.get(name)
+        if (part == null)
+          throw new Error(`BUG: Unknown part ${kind} ${name}`)
+
+        if (part.raw_part == null)
+          throw new Error(`raw_part is empty for ${kind} ${name}`)
+
+        switch (part.kind) {
+          case "widget": {
+            const ast = parse_template(session, part.raw_part)
+            const script = generate_widget(ctx, name, part, ast)
+            process.stdout.write(script);
+            break;
+          }
+          default: {
+            process.stdout.write(`// XXX: dummy\n`);
+            process.stdout.write(`export function ${kind}_${name} {\n`)
+            process.stdout.write(`}\n`)
+          }
         }
+      }
+
+      process.stdout.write(`}\n`);
     }
+  }
 })()
