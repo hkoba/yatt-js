34bec8f388e1cab34bb503b15e16bffcb772786b
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-07-22 15:26:18 +0900

    Add exports

---
+++ src/declaration/index.ts
@@ -0,0 +1,18 @@
+export {
+  build_template_declaration,
+  TemplateDeclaration, PartMapType, RouteMapType,
+  builtin_builders
+} from '../declaration/build'
+
+export {
+  Part, Widget, makeWidget, Action
+} from './part'
+
+export {
+  VarTypeSpec, Variable, WidgetVar, DelegateVar, DefaultFlag
+} from './vartype'
+
+export {
+  BuilderContext, BuilderSession,
+  BuilderMap, DeclarationProcessor
+} from '../declaration/context'
