1ee3110d5839781943d6c298d953e8f033e454a5
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-08-09 18:09:51 +0900

    Add a test for extract_prefix_spec

--- core/lrxml/test/count_lines.ts
+++ core/lrxml/test/count_lines.ts
@@ -2,7 +2,7 @@
 
 import tap from 'tap'
 
-import {lineNumber, extract_line} from '../src/utils/count_lines'
+import {lineNumber, extract_line, extract_prefix_spec} from '../src/utils/count_lines'
 
 tap.same(lineNumber(``), 1)
 
@@ -20,3 +20,7 @@ tap.same(extract_line(`01234`, 0, 2), `01234` + `\n ^`);
 tap.same(extract_line(`foo
 bar
 `, 4, 2), `bar` + `\n ^`);
+
+tap.same(extract_prefix_spec(`foo
+bar
+`, 5), [3, 2, 2]);
