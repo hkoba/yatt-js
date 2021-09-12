72ccc589955580a6f1e0cb229c77977a4d05c030
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-12 11:56:30 +0900

    Move webpack.config.js

--- editor-support/vscode/webpack.config.js
+++ editor-support/vscode/webpack.config.js
@@ -13,7 +13,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = /** @type WebpackConfig */ {
-	context: path.dirname(__dirname),
+	context: __dirname,
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
@@ -55,7 +55,7 @@ module.exports = /** @type WebpackConfig */ {
 	},
 	output: {
 		filename: '[name].js',
-		path: path.join(__dirname, '../dist/web'),
+		path: path.join(__dirname, 'dist/web'),
 		libraryTarget: 'commonjs'
 	},
 	devtool: 'nosources-source-map' // create a source map that points to the original source file
