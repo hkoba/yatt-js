3a2b59d8d3bb9e3243f5f9160dd77565c741b0b3
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-12 12:21:15 +0900

    Rename extension entrypoint

--- editor-support/vscode/webpack.config.js
+++ editor-support/vscode/webpack.config.js
@@ -12,7 +12,7 @@ const webConfig = /** @type WebpackConfig */ {
 	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 	target: 'webworker', // extensions run in a webworker context
 	entry: {
-		'extension': './src/extension.ts',
+		'extension-web': './src/extension.ts',
 		'test/suite/index': './src/web/test/suite/index.ts'
 	},
 	resolve: {
@@ -49,7 +49,7 @@ const webConfig = /** @type WebpackConfig */ {
 		hints: false
 	},
 	output: {
-		filename: '[name]-web.js',
+		filename: '[name].js',
 		path: path.join(__dirname, 'dist/'),
 		libraryTarget: 'commonjs'
 	},
