9816547ab99fca9b10d585335c25307ac3b21adb
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-15 14:38:51 +0900

    Not worked - How can I register editor.codeActionsOnSave?

--- editor-support/vscode/src/extension.ts
+++ editor-support/vscode/src/extension.ts
@@ -2,26 +2,57 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const LANG_ID = 'yatt-js'
+const COMMAND_ID = `${LANG_ID}.lint`
+const ACTION_ID = 'editor.codeActionsOnSave'
+
+let handler
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yatt-js" is now active in the web extension host!');
+  handler = new RunOnSave;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('yatt-js.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  console.log('Congratulations, extension "yatt-js" is now active!');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from yatt-js in a web extension host!');
-	});
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(LANG_ID, handler, {
+      providedCodeActionKinds: RunOnSave.providedCodeActionKinds
+    })
+  )
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(COMMAND_ID, handler.handle.bind(handler))
+  )
+
+  const config = vscode.workspace.getConfiguration(ACTION_ID)
+  if (config) {
+    console.log(`Found ${ACTION_ID}: `, config)
+    config.update(COMMAND_ID, true)
+  } else {
+    console.log(`Not found: ${ACTION_ID}`)
+  }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+export class RunOnSave implements vscode.CodeActionProvider {
+  public static readonly providedCodeActionKinds = [
+    vscode.CodeActionKind.Source
+  ]
+
+  public handle() {
+    const editor = vscode.window.activeTextEditor
+    if (editor != null) {
+      console.log(`curdoc: `, editor.document.getText())
+    }
+  }
+
+  public provideCodeActions(document: vscode.TextDocument, range: vscode.Range)
+  : vscode.CodeAction[] | undefined {
+    console.log(`Provider is called`)
+    return;
+  }
+}
