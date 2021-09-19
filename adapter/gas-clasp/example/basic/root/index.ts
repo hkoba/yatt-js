b4c3c39e86202638d389ca6e01dedab08a0d1594
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-19 15:42:44 +0900

    Generated code

--- adapter/gas-clasp/example/basic/root/index.ts
+++ adapter/gas-clasp/example/basic/root/index.ts
@@ -1,7 +1,18 @@
+namespace $tmpl.another {
+export function render_ (this: typeof $tmpl.another, CON: yatt.runtime.Connection, {BODY}: {BODY?: (CON: yatt.runtime.Connection, {}: {}) => void}) {const $this = this
+ CON.append('<h2>another</h2>\n');
+}
+}
+namespace $tmpl.error {
+export function render_ (this: typeof $tmpl.error, CON: yatt.runtime.Connection, {msg, BODY}: {msg?: string; BODY?: (CON: yatt.runtime.Connection, {}: {}) => void}) {const $this = this
+ CON.append('<h2>Error</h2>\n');
+ CON.appendUntrusted(msg); CON.append('\n');
+}
+}
 namespace $tmpl.index {
-export function render_ (this: typeof $tmpl.index, CON: yatt.runtime.Connection, {BODY}: {BODY?: (CON: yatt.runtime.Connection, {}: {}) => void}) {const $this = this
+export function render_ (this: typeof $tmpl.index, CON: yatt.runtime.Connection, {request, BODY}: {request?: any; BODY?: (CON: yatt.runtime.Connection, {}: {}) => void}) {const $this = this
  $this.render_layout(CON, {title: 'Hello world!', BODY: (CON: yatt.runtime.Connection, {}: {}): void => { CON.append('\n');
- CON.append('Are you ok?\n');
+ CON.append('Are you ok? request=('); CON.appendUntrusted(request); CON.append(')\n');
  CON.append('\n');
 }}); CON.append('\n');
 }
