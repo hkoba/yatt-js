4dbf10ae302251016fc0fdf81de3f3ad64ffc3bd
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-19 13:09:54 +0900

    event.waitUntil was not necessary

--- editor-support/vscode/src/extension.ts
+++ editor-support/vscode/src/extension.ts
@@ -20,17 +20,15 @@ export function activate(context: vscode.ExtensionContext) {
     } catch (err) {
       // XXX: rejected promise not handled within 1 second: Error: Unexpected type
       // Why?
-      event.waitUntil((async () => {
-        console.log(`error: `, err)
-        if (err instanceof TokenError) {
-          console.log(`line ${err.token.line} col ${err.token.column}`)
-          const pos = new vscode.Position(err.token.line - 1, err.token.column - 1)
-          vscode.window.activeTextEditor!.selection = new vscode.Selection(pos, pos)
-        }
-        if (err instanceof Error) {
-          vscode.window.showErrorMessage(err.message)
-        }
-      })())
+      console.log(`error: `, err)
+      if (err instanceof TokenError && vscode.window.activeTextEditor != null) {
+        console.log(`line ${err.token.line} col ${err.token.column}`)
+        const pos = new vscode.Position(err.token.line - 1, err.token.column - 1)
+        vscode.window.activeTextEditor.selection = new vscode.Selection(pos, pos)
+      }
+      if (err instanceof Error) {
+        vscode.window.showErrorMessage(err.message)
+      }
     }
   }
 
