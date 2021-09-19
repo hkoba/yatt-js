64a8decd81e3f2bb32a2085da0b64478b9e4ec5e
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-19 15:35:43 +0900

    wip - dispatch view by pathInfo

--- adapter/gas-clasp/example/basic/root/main.ts
+++ adapter/gas-clasp/example/basic/root/main.ts
@@ -1,11 +1,20 @@
-function doGet(request: GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
+function doGet(request: {pathInfo?: string} & GoogleAppsScript.Events.DoGet): GoogleAppsScript.HTML.HtmlOutput {
   let output = HtmlService.createHtmlOutput()
   let CON = {
     append: output.append.bind(output),
     appendUntrusted: output.appendUntrusted.bind(output)
   }
 
-  $tmpl.index.render_(CON, {})
+  const pathInfo = request.pathInfo ?? "index";
+  const [fileName, ...rest] = pathInfo.split('/')
+
+  // XXX
+  const page = ($tmpl as any)[fileName]
+  if (page == null) {
+    $tmpl.error.render_(CON, {msg: `Page not found: ${fileName}`})
+  } else {
+    page.render_(CON, {})
+  }
 
   return output
 }
