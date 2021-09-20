b816f47b30f8337a9e2a7af9ec8b24d77c052601
Kobayasi, Hiroaki <buribullet@gmail.com>
2021-09-20 14:36:08 +0900

    wip - collect_arg_spec

--- core/codegen0/src/codegen0/macro/foreach.ts
+++ core/codegen0/src/codegen0/macro/foreach.ts
@@ -1,5 +1,6 @@
 import {
   Node
+  , AttItem, AttElement, AttValue, attValue, isBareLabeledAtt
 } from 'lrxml-js'
 
 import {CodeGenContext} from '../context'
@@ -10,5 +11,46 @@ export function macro_foreach(ctx: CodeGenContext, scope: VarScope, node: Node &
 : string
 {
   console.dir(node, {depth: null, color: true})
+  const primary = collect_arg_spec(node.attlist, ['my', 'list'])
+  if (isError(primary))
+    ctx.token_error(primary.value, primary.err)
+  const actualArgs = primary.ok;
+  console.log(`my: `, actualArgs.my)
+  console.log(`list: `, actualArgs.list)
   return ''
+}
+
+function isError<T, E>(value: {ok: T} | {err: string, value: E})
+: value is {err: string, value: E} {
+  return (value as {err: string, value: E}).err !== undefined
+}
+
+function collect_arg_spec<T extends {[k: string]: AttValue}>(
+  attlist: (AttItem | AttElement)[], specList: (keyof T)[]
+)
+: {ok: Partial<T>} | {err: string, value: (AttItem | AttElement)} {
+  const spec: Set<string> = new Set(specList as string[])
+  const seen: Set<string> = new Set
+  const actual: {[k: string]: AttValue} = {}
+  for (const att of attlist) {
+    if (att.kind === "attelem")
+      break;
+    let argName: string | undefined
+    if (! isBareLabeledAtt(att)) {
+      if (seen.size >= specList.length)
+        return {err: `Too many args`, value: att}
+      argName = specList[seen.size] as string
+    } else {
+      argName = att.label.value;
+      if (! spec.has(argName)) {
+        return {err: `Unknown arg ${argName}`, value: att.label}
+      }
+      if (seen.has(argName)) {
+        return {err: `Duplicate arg ${argName}`, value: att.label}
+      }
+    }
+    seen.add(argName)
+    actual[argName] = attValue(att)
+  }
+  return {ok: actual as Partial<T>}
 }
