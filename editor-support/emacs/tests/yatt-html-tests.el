914a2177578545e3697510a9c23cb7264de1d1fe
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-01-12 16:32:05 +0900

    Add minimal tests

--- editor-support/emacs/tests/yatt-html-tests.el
+++ editor-support/emacs/tests/yatt-html-tests.el
@@ -1,2 +1,19 @@
 
 (require 'poly-yatt-html)
+(require 'polymode-test-utils)
+
+;; (pm-test-get-file "t000.ytjs")
+
+(pm-test-run-on-file poly-yatt-html-mode "t000.ytjs"
+  (switch-to-buffer (current-buffer))
+  (goto-char 1)
+  (pm-switch-to-buffer)
+  (should (equal '(1 . 14) (pm-innermost-range (point))))
+  (should (equal '(14 . 188) (poly-yatt-multipart-boundary 1)))
+
+  (should (equal '(14 . 86) (pm-innermost-range (point))))
+
+  (goto-char 109)
+  (should (equal '(86 . 112) (pm-innermost-range (point))))
+
+)
