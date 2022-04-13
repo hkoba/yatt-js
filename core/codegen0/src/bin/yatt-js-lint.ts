3741bcdba5e4d71b10ceb0c807608e4490c5de1f
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-04-13 21:13:25 +0900

    wip - add yatt-js-lint

---
+++ core/codegen0/src/bin/yatt-js-lint.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import {YattConfig} from '../config'
+import * as yattPath from '../path'
+import {generate_namespace} from '../codegen0/namespace/generate'
+import {generate_module} from '../codegen0/module/generate'
+
+import {readFileSync} from 'fs'
+
+import { parse_long_options } from 'lrxml-js'
+
+export function lint(fileList: string[], config: YattConfig) {
+  if (config.rootDir == null) {
+    config.rootDir = yattPath.longestPrefixDir(fileList)
+  }
+
+  const generate = config.templateNamespace ? generate_namespace :
+    generate_module;
+
+  for (const filename of fileList) {
+    const outFn = yattPath.outFileName(filename, '.ts', config)
+    const source = readFileSync(filename, {encoding: 'utf-8'})
+    const _output = generate(source, {filename, ...config})
+  }
+}
+
+
+let args = process.argv.slice(2)
+const debugLevel = parseInt(process.env.DEBUG ?? '', 10) || 0
+let config = {
+  debug: { declaration: debugLevel },
+  // ext: 'ytjs',
+}
+
+parse_long_options(args, {target: config})
+
+lint(args, config);
