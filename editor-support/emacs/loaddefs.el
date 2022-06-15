0f55a072a56e76b004f6b4c3327cfb2358db424e
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-06-15 11:40:26 +0900

    For #1 - split poly-yatt

--- editor-support/emacs/loaddefs.el
+++
@@ -1,54 +0,0 @@
-;;; loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "poly-yatt-config" "poly-yatt-config.el" (0
-;;;;;;  0 0 0))
-;;; Generated autoloads from poly-yatt-config.el
-
-(autoload 'poly-yatt-load-config "poly-yatt-config" nil nil nil)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "poly-yatt-config" '("poly-yatt-")))
-
-;;;***
-
-;;;### (autoloads nil "poly-yatt-html" "poly-yatt-html.el" (0 0 0
-;;;;;;  0))
-;;; Generated autoloads from poly-yatt-html.el
- (autoload 'poly-yatt-html-mode "poly-yatt-html" nil t)
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "poly-yatt-html" '("poly-yatt-")))
-
-;;;***
-
-;;;### (autoloads nil "yatt-js-lint-mode" "yatt-js-lint-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from yatt-js-lint-mode.el
-
-(autoload 'yatt-js-lint-mode "yatt-js-lint-mode" "\
-Lint yatt-js files
-
-If called interactively, enable Yatt-Js-Lint mode if ARG is
-positive, and disable it if ARG is zero or negative.  If called
-from Lisp, also enable the mode if ARG is omitted or nil, and
-toggle it if ARG is `toggle'; disable the mode otherwise.
-
-\(fn &optional ARG)" t nil)
-
-(autoload 'yatt-js-lint-run "yatt-js-lint-mode" nil t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("subdirs.el") (0 0 0 0))
-
-;;;***
-
-(provide 'loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; loaddefs.el ends here
--- editor-support/emacs/targets/local.el
+++
@@ -1,5 +0,0 @@
-
-;; add all poly* directories from the parent directory to load path
-(let ((poly-dirs (directory-files (file-name-directory (directory-file-name default-directory))
-                                  t "^poly")))
-  (setq load-path (append poly-dirs load-path)))
