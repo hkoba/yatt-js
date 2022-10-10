444bba5a145dba0c5bd7a138afaf0d8a0a313c25
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-10-10 13:11:09 +0900

    wip - change generate_entity

--- core/codegen0/src/codegen0/widget/entity/generate.ts
+++ core/codegen0/src/codegen0/widget/entity/generate.ts
@@ -1,73 +1,86 @@
 import {Node, EntPathItem, EntTerm} from 'lrxml'
 import {CodeGenContext} from '../../context'
-import {Widget} from '../../../declaration'
 import {VarScope} from '../../varscope'
-import type {Printable} from './types'
 import {escapeAsStringLiteral} from '../../escape'
 
-export function generate_entity(
-  ctx: CodeGenContext, scope: VarScope, node: Node & {kind: 'entity'}
-) {
-  if (node.path.length !== 1) {
-    ctx.NIMPL()
-  }
+import {CodeFragment, joinAsArray} from '../../codefragment'
+import type {Argument} from '../../template_context/'
 
-  return generate_entpath(ctx, scope, node.path)
+export function generate_entity(
+  ctx: CodeGenContext, scope: VarScope, node: Node & {kind: 'entity'},
+  options?: {need_runtime_escaping?: boolean}
+): Argument {
+  return generate_entpath(ctx, scope, node.path, options ?? {})
 }
 
 export function generate_entpath(
-  ctx: CodeGenContext, scope: VarScope, path: EntPathItem[]
-): Printable {
+  ctx: CodeGenContext, scope: VarScope, path: EntPathItem[],
+  {need_runtime_escaping}: {need_runtime_escaping?: boolean}
+): Argument {
 
   const [head, ...rest] = path
 
-  const result: string[] = []
+  if (rest.length) {
+    ctx.NIMPL(rest[0])
+  }
+  const result: CodeFragment[] = []
   switch (head.kind) {
     case 'var': {
       const variable = scope.lookup(head.name)
-      if (variable == null)
-        ctx.token_error(head, `No such variable: ${head.name}`);
-
-      if (! rest.length)
-        return {kind: "var", variable}
-
-      result.push(head.name)
+      if (variable != null) {
+        // typeName
+        result.push({kind: 'name', code: variable.varName, source: head})
+        if (variable.typeName === "html")
+          need_runtime_escaping = false;
+      } else {
+        const fn = ctx.session.entFns[head.name]
+        if (fn == null) {
+          ctx.token_error(head, `No such variable: ${head.name}`);
+        }
+        result.push(ctx.entFnPrefix(), ".", {kind: 'name', code: head.name, source: head}, ".apply(CON, [])");
+      }
       break;
     }
     case 'call': {
-      const fn = ctx.session.entFns[head.name]
-      if (fn == null)
-        ctx.token_error(head, `No such entity function: ${head.name}`)
-      const args = generate_entlist(ctx, scope, head.elements)
-      result.push(`${ctx.entFnPrefix()}.${head.name}.apply(CON, [${args}])`);
+      // XXX: entmacro
+      const variable = scope.lookup(head.name)
+      if (variable != null) {
+        const args = generate_entlist(ctx, scope, head.elements, {})
+        result.push({kind: 'name', code: head.name, source: head}, "(", args, "})")
+      } else {
+        const fn = ctx.session.entFns[head.name]
+        if (fn == null) {
+          ctx.token_error(head, `No such variable: ${head.name}`);
+        }
+        // 
+        const args = generate_entlist(ctx, scope, head.elements, {})
+        result.push(`${ctx.entFnPrefix()}.`,
+                    {kind: 'name', code: head.name, source: head},
+                    `.apply(CON, [`, args, `])`);
+      }
       break;
     }
     default:
       ctx.NIMPL(head)
   }
 
-  if (rest.length) {
-    ctx.NIMPL(rest[0])
-  }
-
   return {
-    kind: "expr",
-    text: result.join('.')
+    kind: "argument", need_runtime_escaping,
+    items: joinAsArray('.', result)
   }
 }
 
 export function generate_entlist(
-  ctx: CodeGenContext, scope: VarScope, nodeList: EntTerm[]
-): string {
-  const exprList = nodeList.map(term => {
+  ctx: CodeGenContext, scope: VarScope, nodeList: EntTerm[],
+  options: {need_runtime_escaping?: boolean}
+): CodeFragment {
+  const exprList: CodeFragment[] = nodeList.map(term => {
     if (term instanceof Array) {
-      const pathExpr = generate_entpath(ctx, scope, term)
-      if (pathExpr.kind === "var")
-        return pathExpr.variable.varName
-      return pathExpr.text
+      return generate_entpath(ctx, scope, term, options).items
     } else {
-      return escapeAsStringLiteral(term.text)
+      return {kind: 'other', code: escapeAsStringLiteral(term.text)
+              , source: term}
     }
   })
-  return exprList.join(', ')
+  return joinAsArray(', ', exprList)
 }
