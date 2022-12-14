baad3d4b802cf90698466c62d27eb71f4da5647a
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-12-14 15:08:35 +0900

    Fix codegen of `<!yatt:entity>`

--- core/codegen0/src/codegen0/entity/generate.ts
+++ core/codegen0/src/codegen0/entity/generate.ts
@@ -2,23 +2,19 @@ import {Entity} from '../../declaration'
 
 import {CodeGenContext} from '../context'
 
-import {generate_argdecls} from '../widget/argdecls'
+import {CodeFragment, joinAsArray} from '../codefragment'
 
-import {VarScope} from '../varscope'
-
-import {CodeFragment} from '../codefragment'
+import {varTypeExpr} from '../widget/vartype'
 
 export function generate_entity(ctx: CodeGenContext<Entity>): CodeFragment {
   let program: CodeFragment = [
-    `export function entity_`,
+    `export function `,
     {kind: 'name', code: ctx.part.name, source: ctx.part.nameNode},
-    ` `
   ]
 
-  const dummyScope = new VarScope
-  const argDecls = generate_argdecls(ctx, dummyScope, ctx.part);
+  const argDecls = generate_entity_argdecls(ctx, ctx.part)
 
-  program.push("(", argDecls, ") {\n");
+  program.push("(", joinAsArray(', ', argDecls), ") {\n");
 
   for (const item of ctx.part.raw_part!.payload) {
     if (item.kind === "text") {
@@ -29,4 +25,22 @@ export function generate_entity(ctx: CodeGenContext<Entity>): CodeFragment {
   program.push("\n}")
 
   return program
+}
+
+export function generate_entity_argdecls(
+  ctx: CodeGenContext<Entity>,
+  entityDecl: Entity
+): CodeFragment[] {
+
+  let args: CodeFragment[] = []
+
+  for (const [name, spec] of entityDecl.argMap.entries()) {
+    const label = spec.attItem?.label
+    args.push([{kind: 'name', code: name, source: label}
+               , ': '
+               , {kind: 'name', code: varTypeExpr(ctx, spec)}
+              ])
+  }
+
+  return args
 }
