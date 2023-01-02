27050912a6502f281ff50902124637d7ce52ed46
Kobayasi, Hiroaki <buribullet@gmail.com>
2023-01-02 17:28:08 +0900

    sourcemap for argument declarations (not fully tested)

--- core/codegen0/src/codegen0/widget/argdecls.ts
+++ core/codegen0/src/codegen0/widget/argdecls.ts
@@ -1,23 +1,37 @@
+import {Node} from 'lrxml'
 import {CodeGenContext, Part} from '../context'
 import {VarScope} from '../varscope'
 import {varTypeExpr} from './vartype'
 
+import {CodeFragment, joinAsArray} from '../codefragment'
+
 export function generate_argdecls<T extends Part>(
   ctx: CodeGenContext<T>, _scope: VarScope, widget: T
-): string {
-  const args = []
-  const types = []
+): CodeFragment {
+  const args: CodeFragment[] = []
+  const types: CodeFragment[] = []
   for (const [name, varSpec] of widget.argMap.entries()) {
-    args.push(name); // XXX: default value
-    const opt = (() => {
-      if (varSpec.defaultSpec && varSpec.defaultSpec[0] === "!") {
-        return ""
-      }
-      return "?";
-    })()
-    // XXX: readonly?
-    const typeExpr = varTypeExpr(ctx, varSpec)
-    types.push(`${name}${opt}: ${typeExpr}`);
+    // XXX: default value
+    const nameCode: CodeFragment = {
+      kind: 'name', code: name, source: varSpec.attItem
+    }
+    args.push(nameCode);
+
+    const typeAnot: CodeFragment[] = []
+    typeAnot.push(nameCode)
+    if (!(varSpec.defaultSpec && varSpec.defaultSpec[0] === "!")) {
+      typeAnot.push("?")
+    }
+    typeAnot.push(`: `, varTypeExpr(ctx, varSpec))
+
+    types.push(typeAnot)
+
   }
-  return `{${args.join(', ')}}: {${types.join('; ')}}`
+  return [
+    "{",
+    joinAsArray(', ', args),
+    "}: {",
+    joinAsArray('; ', types),
+    "}"
+  ]
 }
