efad49a9d1a4b155130a140f7adccfd772e5e23a
Kobayasi, Hiroaki <buribullet@gmail.com>
2024-04-27 12:23:58 +0900

    rewrite with @cross/test

--- core/codegen0/test/run0.ts
+++ core/codegen0/test/run0.ts
@@ -1,19 +1,20 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env -S deno run -A
 
-import tap from 'tap'
+import {test} from "@cross/test"
+import {assertEquals} from '@std/assert'
 
-import {runFile as runFileNS} from '../src/codegen0/namespace/run0'
+import {runFile as runFileNS} from '../src/codegen0/namespace/run0.ts'
 
-import {runFile as runFileMod} from '../src/codegen0/module/run0'
+import {runFile as runFileMod} from '../src/codegen0/module/run0.ts'
 
-import {rootname} from '../src/path'
+import {rootname} from '../src/path.ts'
 
 {
-  const dir = rootname(__filename) + '.d/'
+  const dir = rootname(new URL(import.meta.url).pathname) + '.d/'
 
-  tap.test(`namespace`, t => {
+  test(`namespace`, () => {
     const testFile = (filename: string, expect: string) =>
-      t.same(runFileNS(dir + filename, {connectionTypeName: 'yatt.runtime.Connection'}), expect, filename);
+      assertEquals(runFileNS(dir + filename, {connectionTypeName: 'yatt.runtime.Connection'}), expect, filename);
     
     testFile("widget.ytjs", `<h2>3</h2>
 <div>hoehoe</div>
@@ -22,13 +23,11 @@ aaa
 `)
 
     testFile("entity.ytjs", `11\n`)
-
-    t.end()
   })
 
-  tap.test(`module`, t => {
+  test(`module`, () => {
     const testFile = (filename: string, expect: string) =>
-      t.same(runFileMod(dir + filename, {connectionTypeName: 'yatt.runtime.Connection'}), expect, filename);
+      assertEquals(runFileMod(dir + filename, {connectionTypeName: 'yatt.runtime.Connection'}), expect, filename);
     
     testFile("widget.ytjs", `<h2>3</h2>
 <div>hoehoe</div>
@@ -37,8 +36,5 @@ aaa
 `)
 
     testFile("entity.ytjs", `11\n`)
-
-    t.end()
   })
-
 }
