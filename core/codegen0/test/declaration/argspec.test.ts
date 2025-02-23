f76a3c9b5821d8bde3468af88924bd83a6c5267c
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-02-23 12:36:25 +0900

    Add argspec test

---
+++ core/codegen0/test/declaration/argspec.test.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S deno test -A
+
+import {test as cross_test} from "@cross/test"
+import {assertEquals} from '@std/assert'
+
+import {parse_arg_spec} from "../../src/declaration/addArgs.ts"
+
+import type { BuilderContext } from '../../src/declaration/context.ts'
+
+{
+  const dummyCtx = null as unknown as BuilderContext;
+
+  cross_test("empty string", () => {
+    assertEquals(parse_arg_spec(dummyCtx, "", 'text'), {
+      typeName: 'text'
+    })
+  })
+
+  function test(input: string, callback: (s: string) => any, expected: any) {
+    cross_test(input, () => {
+      assertEquals(callback(input), expected)
+    })
+  }
+
+  test('text?my default value'
+    , (s) => parse_arg_spec(dummyCtx, s, 'text')
+    , {
+      typeName: 'text',
+      defaultSpec: ['?', "my default value"]
+    });
+
+  test('value!'
+    , (s) => parse_arg_spec(dummyCtx, s, 'text')
+    , {
+      typeName: 'value',
+      defaultSpec: ['!', ""]
+    }
+  )
+}
