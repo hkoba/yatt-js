fd35e57f71b2e80f6970ac4193cf5ad394654bda
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-03-16 12:31:02 +0900

    export generate_mounter

--- core/codegen0/src/codegen0/index.ts
+++ core/codegen0/src/codegen0/index.ts
@@ -4,4 +4,6 @@ export {generate_module} from './module/generate.ts'
 
 export {generate_namespace} from './namespace/generate.ts'
 
+export {generate_mounter} from './mounter/generate.ts'
+
 export {runFile} from './mounter/runner.ts'
