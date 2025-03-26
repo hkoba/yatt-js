8a8cf589b67afbf559f8c6766fef00ca2fd47519
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-03-26 17:49:09 +0900

    Fix filehandle leakage

--- core/codegen0/src/declaration/partFolder.ts
+++ core/codegen0/src/declaration/partFolder.ts
@@ -1,53 +1,55 @@
 import * as Path from 'node:path'
 import * as Fs from 'node:fs/promises'
 
-import type {BuilderBaseSession, DeclTree} from './context.ts'
+import type {BuilderBaseSession} from './context.ts'
 
 export async function needsUpdate(
-  session: BuilderBaseSession, cache: DeclTree,
-  virtPath: string, realPath: string
+  session: BuilderBaseSession, realPath: string
 ): Promise<{modTime: number, source: string} | undefined> {
   const debug = session.params.debug.declaration
   if (debug) {
-    console.log(`refreshing virtPath=${virtPath}, realPath=${realPath}`)
+    console.log(`refreshing realPath=${realPath}`)
   }
   if (session.visited.get(realPath)) {
     if (debug) {
-      console.log(` => has visited: ${virtPath}`)
+      console.log(` => has visited: ${realPath}`)
     }
     return
   }
+
   let fh, stat;
   try {
     fh = await Fs.open(realPath)
     stat = await fh.stat()
-  } catch (err) {
-    if (debug) {
-      console.log(` => Not found: `, err)
-    }
-  }
 
-  if (!fh || !stat) {
-    if (debug) {
-      console.log(` => No real file: ${realPath}`)
-    }
-    return;
-  } else if (cache.has(virtPath)) {
-    if (debug) {
-      console.log(` => has cache: ${virtPath}`)
-    }
-    const entry = cache.get(virtPath)!
-    if (stat.mtimeMs <= entry.modTime) {
+    if (session.declCache.has(realPath)) {
       if (debug) {
-        console.log(` => cache is valid: cache(${entry.modTime}) vs stat(${stat.mtimeMs})`)
+        console.log(` => has cache: ${realPath}`)
       }
-      return
+      const entry = session.declCache.get(realPath)!
+      if (stat.mtimeMs <= entry.modTime) {
+        if (debug) {
+          console.log(` => cache is valid: cache(${entry.modTime}) vs stat(${stat.mtimeMs})`)
+        }
+        return
+      }
     }
+    if (debug) {
+      console.log(` => Reading ${realPath}`)
+    }
+
+    const source = await fh.readFile({encoding: 'utf-8'})
+    return {source, modTime: stat!.mtimeMs}
+
+  } catch (err) {
+    if (err instanceof Error && err.name !== "NotFound") {
+      console.warn(err)
+    } else if (debug) {
+      console.log(` => Not found: ${realPath}`)
+    }
+  } finally {
+    await fh?.close()
   }
-
-  const source = await fh.readFile({encoding: 'utf-8'})
-
-  return {source, modTime: stat!.mtimeMs}
 }
 
 // partPath = ['foo', 'bar']
@@ -57,59 +59,66 @@ export async function needsUpdate(
 // 1. foo.ytjs:<!yatt:widget bar>
 export function* candidatesForLookup(
   session: BuilderBaseSession, fromDir: string, partPath: string[]
-): Generator<{rootDir: string, realPath: string, virtPath: string, name: string, cache: DeclTree}> {
+): Generator<{realPath: string, name: string}> {
 
   const debug = session.params.debug.declaration ?? 0
 
-  const ext = session.params.ext_public
+  if (debug) {
+    console.log(`fromDir: ${fromDir} looking partPath: `, partPath)
+  }
 
-  const [[rootDir, primaryMap], ...restCache] = Object.entries(session.declCacheSet)
+  const ext = session.params.ext_public
 
   const genList = session.params.lookup_subdirectory_first ?
     [partInSubdir, partInFile] :
     [partInFile, partInSubdir];
 
+  // XXX: ext_public, ext_private でループ
   {
-    const absFromDir = Path.resolve(fromDir)
+    const absFromDir = Path.resolve(fromDir) + Path.sep
 
     for (const gen of genList) {
       yield {
-        ...gen(debug, absFromDir, partPath, ext, rootDir),
-        cache: primaryMap
+        ...gen(debug, absFromDir, partPath, ext),
       }
     }
   }
 
-  for (const [dir, cache] of restCache) {
-    const absDir = Path.resolve(dir)
-    for (const gen of genList) {
-      yield {
-        ...gen(debug, absDir, partPath, ext),
-        cache
-      }
+}
+
+// when partPath is ['foo', 'bar']...
+
+// look for 'foo.ytjs'
+function partInFile(
+  debug: number, fromDir: string, partPath: string[]
+  , ext: string
+): {realPath: string, name: string} {
+  if (partPath.length === 1) {
+    const realPath = Path.resolve(fromDir, partPath[0]) + ext
+    const result = {realPath, name: ""}
+    return result
+  } else {
+    const virtPath = partPath.slice(0, partPath.length-1).join(Path.sep);
+    const realPath = Path.resolve(fromDir, virtPath) + ext
+    const result = {realPath, name: partPath[partPath.length-1]}
+    if (debug) {
+      console.log(`partInFile: fromDir: ${fromDir} virtPath: ${virtPath} ext=${ext} =>`, result)
     }
+    return result
   }
+
 }
 
-function partInFile(debug: number, fromDir: string, partPath: string[], ext: string, rootDir?: string): {rootDir: string, realPath: string, virtPath: string, name: string} {
+// look for 'foo/bar.ytjs'
+function partInSubdir(
+  debug: number, fromDir: string, partPath: string[]
+  , ext: string
+): {realPath: string, name: string} {
+  const virtPath = partPath.join(Path.sep)
+  const realPath = Path.resolve(fromDir, virtPath) + ext
+  const result = {realPath, name: ''};
   if (debug) {
-    console.log(`InFile rootDir=${rootDir}, fromDir=${fromDir}`)
+    console.log(`partInSubdir: virtPath: ${virtPath} ext=${ext} => `, result)
   }
-
-  const virtPath = [fromDir, ...partPath.slice(0, partPath.length-1)].
-    join(Path.sep);
-  const folder = rootDir ?? fromDir
-  const realPath = Path.resolve(folder, virtPath) + ext
-  return {realPath, virtPath, rootDir: folder, name: partPath[partPath.length-1]}
-}
-
-function partInSubdir(debug: number, fromDir: string, partPath: string[], ext: string, rootDir?: string): {rootDir: string, realPath: string, virtPath: string, name: string} {
-  if (debug) {
-    console.log(`InSubdir rootDir=${rootDir}, fromDir=${fromDir}`)
-  }
-  const virtPath = [fromDir, ...partPath].
-    join(Path.sep)
-  const folder = rootDir ?? fromDir
-  const realPath = Path.resolve(folder, virtPath) + ext
-  return {realPath, virtPath, rootDir: folder, name: ''}
+  return result
 }
