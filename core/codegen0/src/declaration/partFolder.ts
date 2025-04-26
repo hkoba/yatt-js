f50984d55a5c2ff359f4266d4e5b9c4fc97090c0
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-04-26 16:18:16 +0900

    onmemory test の場合を特別扱い

--- core/codegen0/src/declaration/partFolder.ts
+++ core/codegen0/src/declaration/partFolder.ts
@@ -19,11 +19,19 @@ export function* candidatesForLookup(
 
   const extMayList = session.params.ext_public
 
+  if (fromDir === "") {
+    for (const ext of Array.isArray(extMayList) ? extMayList : [extMayList]) {
+      // XXX: fromDir を使わないのに渡すのは、気持ち悪い…
+      yield partInFile(debug, fromDir, partPath, ext, true)
+    }
+    return;
+  }
+
   const genList = session.params.lookup_subdirectory_first ?
     [partInSubdir, partInFile] :
     [partInFile, partInSubdir];
 
-  // XXX: ext_public, ext_private でループ
+  // XXX: ext_private でもループ
   {
     const absFromDir = Path.resolve(fromDir) + Path.sep
 
@@ -43,15 +51,15 @@ export function* candidatesForLookup(
 // look for 'foo.ytjs'
 function partInFile(
   debug: number, fromDir: string, partPath: string[]
-  , ext: string
+  , ext: string, ignoreFromDir?: boolean
 ): {realPath: string, name: string} {
   if (partPath.length === 1) {
-    const realPath = Path.resolve(fromDir, partPath[0]) + ext
+    const realPath = (ignoreFromDir ? partPath[0] : Path.resolve(fromDir, partPath[0])) + ext
     const result = {realPath, name: ""}
     return result
   } else {
     const virtPath = partPath.slice(0, partPath.length-1).join(Path.sep);
-    const realPath = Path.resolve(fromDir, virtPath) + ext
+    const realPath = (ignoreFromDir ? virtPath : Path.resolve(fromDir, virtPath)) + ext
     const result = {realPath, name: partPath[partPath.length-1]}
     if (debug) {
       console.log(`partInFile: fromDir: ${fromDir} virtPath: ${virtPath} ext=${ext} =>`, result)
