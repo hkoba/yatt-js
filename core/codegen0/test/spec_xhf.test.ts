73e609a0d806996f7a85bb30dd55fe838c489f69
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-04-26 15:04:13 +0900

    rename to spec_xhf.test.ts

--- core/codegen0/test/spec_xhf.test.ts
+++ core/codegen0/test/spec_xhf.test.ts
@@ -4,5 +4,7 @@ import {runtests} from '../src/xhftest.ts'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
-await runtests([`${__dirname}/1-basic.xhf`], {})
+await runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
+  ext_public: ['.ytjs', '.yatt', '.html']
+})
 
