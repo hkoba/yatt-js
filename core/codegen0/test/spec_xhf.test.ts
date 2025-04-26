49014ae36a713ce83d44f0fb03b6465eee07004e
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-04-26 16:39:17 +0900

    Remove await

--- core/codegen0/test/spec_xhf.test.ts
+++ core/codegen0/test/spec_xhf.test.ts
@@ -4,7 +4,8 @@ import {runtests} from '../src/xhftest.ts'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
-await runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
-  ext_public: ['.ytjs', '.yatt', '.html']
+runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
+  ext_public: ['.ytjs', '.yatt', '.html'],
+  body_argument_name: 'body'
 })
 
