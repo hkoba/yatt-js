55070e0677fad474aa5d17e9837db5012de35195
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-04-26 16:52:26 +0900

    Temporarily hide xhftest

--- core/codegen0/test/spec_xhf.test.ts
+++ core/codegen0/test/spec_xhf.test.ts
@@ -4,8 +4,12 @@ import {runtests} from '../src/xhftest.ts'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
-runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
-  ext_public: ['.ytjs', '.yatt', '.html'],
-  body_argument_name: 'body'
-})
+import * as process from 'node:process'
+
+if (process.env["TEST_TODO"]) {
+  runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
+    ext_public: ['.ytjs', '.yatt', '.html'],
+    body_argument_name: 'body'
+  })
+}
 
