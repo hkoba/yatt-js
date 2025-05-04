fd68ca696d51ab3fea32c373f5b8d001586f2484
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-05-04 11:54:42 +0900

    Rewrite with await using

--- core/codegen0/src/declaration/load.ts
+++ core/codegen0/src/declaration/load.ts
@@ -6,23 +6,22 @@ export async function loadIfModified(
     path: string, modTimeMs?: number, debug?: number
 ): Promise<RegistryEntry | undefined> {
 
-  let fh
   try {
-    fh = await open(path)
+    await using fh = await open(path)
     let stat = await fh.stat()
 
     if (modTimeMs == null) {
       if (debug) {
-        console.log(`Cache: First fetch: ${path}`)
+        console.log(`loadIfModified: First fetch: ${path}`)
       }
     } else if (stat.mtimeMs <= modTimeMs) {
       if (debug) {
-        console.log(`Cache: Latest: ${path}`)
+        console.log(`loadIfModified: Latest: ${path}`)
       }
       return;
     } else {
       if (debug) {
-        console.log(`Cache: Need update: ${path}`)
+        console.log(`loadIfModified: Need update: ${path}`)
       }
     }
     const source = await fh.readFile({encoding: 'utf-8'})
@@ -31,12 +30,10 @@ export async function loadIfModified(
     return {source, modTimeMs: stat.mtimeMs}
   } catch (err) {
     if (err instanceof Error && err.name !== "NotFound") {
-      console.warn(err)
+      throw err;
     } else if (debug) {
-      console.log(`Cache: Can't read: ${path}`, err)
+      console.log(`loadIfModified: NotFound: ${path}`, err)
     }
-  } finally {
-    await fh?.close()
   }
 }
 
