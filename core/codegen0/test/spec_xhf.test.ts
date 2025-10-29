c948fad71e62bd25dde6abc05f4d671afa79b189
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-10-29 13:51:39 +0900

    Enable xhftest by default

--- core/codegen0/test/spec_xhf.test.ts
+++ core/codegen0/test/spec_xhf.test.ts
@@ -6,14 +6,11 @@ const __dirname = new URL('.', import.meta.url).pathname
 
 import * as process from 'node:process'
 
-if (process.env["TEST_TODO"]) {
-  runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
-    debug: {
-      codegen: parseInt(process.env["DEBUG_CGEN"] ?? "0", 10),
-      declaration: parseInt(process.env["DEBUG_DECL"] ?? "0", 10)
-    },
-    ext_public: ['.ytjs', '.yatt', '.html'],
-    body_argument_name: 'body'
-  })
-}
-
+runtests([`${__dirname}/spec_xhf/1-basic.xhf`], {
+  debug: {
+    codegen: parseInt(process.env["DEBUG_CGEN"] ?? "0", 10),
+    declaration: parseInt(process.env["DEBUG_DECL"] ?? "0", 10)
+  },
+  ext_public: ['.ytjs', '.yatt', '.html'],
+  body_argument_name: 'body'
+})
