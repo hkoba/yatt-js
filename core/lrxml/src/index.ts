c6a9a0098abc2e95b22bb3a2b91291ca76645bdf
Kobayasi, Hiroaki <buribullet@gmail.com>
2026-08-11 11:00:07 +0900

    attribute item type refinement - step2 - better type assertions

--- core/lrxml/src/index.ts
+++ core/lrxml/src/index.ts
@@ -32,8 +32,16 @@ export {
   maybePassThruVarName, maybeArgName
 } from './template/index.ts'
 
-export type {AttItem, AttValue} from './attlist/parse.ts'
-export { attValue, attInnerRange } from './attlist/parse.ts'
+export type {
+  AttItem, AttValue, Label,
+  AttLabeled, AttLabeledByIdent, AttLabeledNested, AttPositional,
+  AttIdentOnly, StringTerm, QuotedStringTerm, BareStringTerm,
+  IdentplusTerm, NestedTerm, EntTermWComment, StringishTerm
+} from './attlist/parse.ts'
+export {
+  attValue, attInnerRange, attKindIsQuotedString, isLabelTerm,
+  termIsStringish
+} from './attlist/parse.ts'
 
 export type { AttStringItem } from './attstring/parse.ts'
 export { parse_attstring } from './attstring/parse.ts'
